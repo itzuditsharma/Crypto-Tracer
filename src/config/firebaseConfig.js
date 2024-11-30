60ce2fdb5540e6b89fe493b8ecf989138bc01a06
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2024-11-30 19:58:48 +0530

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,8 +3,8 @@ const firebaseConfig = {
     authDomain: "crypto-hunter-a0856.firebaseapp.com",
     projectId: "crypto-hunter-a0856",
     storageBucket: "crypto-hunter-a0856.appspot.com",
-    messagingSenderId: "1026396249583",
-    appId: "1:1026396249583:web:dd1961f044eed3275eaa1a"
+    messagingSenderId: "YOUR",
+    appId: "1:1026396249583:web:dd1961f*******75eaa1a"
   };
 
   export default firebaseConfig;
